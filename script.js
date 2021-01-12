090d6d9e4a8b4b32abb293d1f592346689b2646c
Cassio Dos Santos <cassioms764@gmail.com>
2021-01-12 11:23:51 -0300

    criando interação

--- script.js
+++ script.js
@@ -9,9 +9,22 @@ function jump(){
     let position = 0;
 
     let upInterval = setInterval(() => {
+        if (position >= 150){
+            //down
+            clearInterval(upInterval)
+            let downInterval = setInterval(()=>{
+            if (position <= 0){
+                clearInterval(downInterval);
+            } else {
+                position -= 20;
+                dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        } else {
+        //Up
         position += 20;
-
         dino.style.bottom = position + 'px';
+        }
     }, 20);
 }
 
