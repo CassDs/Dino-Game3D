958c2c9182e01b60c6ccae065cbc19e6c2a1c923
Cassio Dos Santos <cassioms764@gmail.com>
2021-01-12 12:30:30 -0300

    finalizando game

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event){
     if(event.keyCode === 32){
@@ -11,7 +12,6 @@ function handleKeyUp(event){
 }
 
 function jump(){
-    let position = 0;
     isJumping = true;
     let upInterval = setInterval(() => {
         if (position >= 150){
@@ -37,7 +37,7 @@ function jump(){
 function createCactus() {
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
-    let randomTime = Math.random() * 6000; 
+    let randomTime = Math.random() * 6000;
 
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
@@ -47,12 +47,15 @@ function createCactus() {
         if (cactusPosition < -60) {
             clearInterval(leftInterval);
             background.removeChild(cactus);
-        } else {
+        } else if(cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim de Jogo</h1>';
+        }else {
             cactusPosition -= 10;
             cactus.style.left = cactusPosition + 'px';
         }
     }, 20);
-    setTimeout(createCactus, randomTime);
+    setTimeout(createCactus, randomTime );
 }
 
 createCactus();
